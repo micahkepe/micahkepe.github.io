c8a1da584e8d6ae3a8645a784fa9c0a0dce81876
MicahKepe <mik3@rice.edu>
2023-06-08 15:32:13 -0600

    Trying different style for website

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
-// Smooth scrolling for navigation links
+// Toggle mobile navigation
 document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('nav ul li a');
-  
-    for (const link of links) {
-      link.addEventListener('click', smoothScroll);
-    }
-  
-    function smoothScroll(event) {
-      event.preventDefault();
-      const targetId = this.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
-      const targetPosition = targetElement.offsetTop;
-  
-      window.scrollTo({
-        top: targetPosition,
-        behavior: 'smooth'
-      });
-    }
+  const navToggle = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('.nav__menu');
+
+  navToggle.addEventListener('click', function() {
+    navMenu.classList.toggle('show');
   });
-  
-  // Toggle mobile navigation
-  document.addEventListener('DOMContentLoaded', function() {
-    const navToggle = document.querySelector('.nav-toggle');
-    const navMenu = document.querySelector('nav ul');
-  
-    navToggle.addEventListener('click', function() {
-      navMenu.classList.toggle('show');
-    });
-  });
-  
+});
