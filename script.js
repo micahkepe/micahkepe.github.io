39c553d272719e667a2862231e4093480db50d6a
MicahKepe <mik3@rice.edu>
2023-06-10 16:52:38 -0600

    Remove old index.html, script.js, and styles.css files

--- script.js
+++
@@ -1,9 +0,0 @@
-// Toggle mobile navigation
-document.addEventListener('DOMContentLoaded', function() {
-  const navToggle = document.querySelector('.nav-toggle');
-  const navMenu = document.querySelector('.nav__menu');
-
-  navToggle.addEventListener('click', function() {
-    navMenu.classList.toggle('show');
-  });
-});
