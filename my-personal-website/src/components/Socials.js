51cc3e41ca7659d382e84648149d03ee372efa9b
MicahKepe <mik3@rice.edu>
2023-06-10 22:58:29 -0600

    Added socials logos and links

--- my-personal-website/src/components/Socials.js
+++ my-personal-website/src/components/Socials.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import GithubIcon from '../icons/GithubLogo';
+import LinkedinIcon from '../icons/LinkedinLogo';
+import InstagramIcon from '../icons/InstagramLogo';
 
 const Socials = () => {
   return (
-    <section id="socials">
-      <h2>Socials</h2>
-      Add the content for the Socials section
-    </section>
+    <div className="socials">
+      <a
+        href="https://github.com/micahkepe"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GithubIcon className="social-icon" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/micah-kepe/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <LinkedinIcon className="social-icon" />
+      </a>
+      <a
+        href="https://www.instagram.com/micahkepe/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <InstagramIcon className="social-icon" />
+      </a>
+    </div>
   );
 };
 
