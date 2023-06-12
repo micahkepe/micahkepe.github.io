2396e755ac39977c6e71e04ac395e73ce4e4753d
MicahKepe <mik3@rice.edu>
2023-06-12 08:59:46 -0600

    Modified QuickAbout text, more layout

--- my-personal-website/src/components/QuickAbout.js
+++ my-personal-website/src/components/QuickAbout.js
@@ -4,8 +4,8 @@ const QuickAbout = () => {
   return (
     <section className="quick-about">
       <h1>Micah Kepe</h1>
-      <p>Current Role</p>
-      <p>Blurb: Short blurb about what you do</p>
+      <p>Computer Science Student at Rice University</p>
+      <p>Dedicated to delivering high-quality solutions.</p>
     </section>
   );
 };
