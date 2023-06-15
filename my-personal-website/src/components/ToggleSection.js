b2c39953a15ad092a9e5a623920fe603a0c92270
MicahKepe <mik3@rice.edu>
2023-06-15 14:29:39 -0600

    ToggleSection styling

--- my-personal-website/src/components/ToggleSection.js
+++ my-personal-website/src/components/ToggleSection.js
@@ -1,40 +1,3 @@
-// import React from 'react';
-// import './ToggleSection.css';
-
-// const ToggleSection = ({ sections }) => {
-//   const handleSectionClick = (section) => {
-//     const sectionElement = document.getElementById(section);
-//     const quickAbout = document.getElementById('quick-about');
-
-//     if (sectionElement && quickAbout) {
-//       const quickAboutHeight = quickAbout.getBoundingClientRect().height;
-//       const sectionPosition = sectionElement.getBoundingClientRect().top;
-
-//       window.scrollTo({
-//         top: sectionPosition - quickAboutHeight,
-//         behavior: 'smooth',
-//       });
-//     }
-//   };
-
-//   return (
-//     <div className="toggle-section indent-3">
-//       {/* Render toggleable sections */}
-//       {sections.map((section) => (
-//         <div
-//           key={section}
-//           className="section-item"
-//           onClick={() => handleSectionClick(section)}
-//         >
-//           <span className="section-name">{section}</span>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ToggleSection;
-
 import React, { useState } from "react";
 
 const ToggleSection = ({ sections }) => {
@@ -69,17 +32,21 @@ const ToggleSection = ({ sections }) => {
       {sections.map((section) => (
         <div
           key={section}
-          className="flex items-center mb-3 cursor-pointer text-sm"
+          className="flex items-center mb-3 cursor-pointer text-sm font-semibold"
           onClick={() => handleSectionClick(section)}
           onMouseEnter={() => handleSectionMouseEnter(section)}
           onMouseLeave={handleSectionMouseLeave}
         >
           <div
             className={`h-1 w-8 bg-skyblue mr-2 transition-all duration-300 ${
-              hoveredSection === section ? "font-bold" : ""
+              hoveredSection === section ? "" : ""
             }`}
           ></div>
-          <span className={`${hoveredSection === section ? "font-bold" : ""}`}>
+          <span
+            className={`${
+              hoveredSection === section ? "hover:text-white" : "text-slate"
+            }`}
+          >
             ⸻ {section.toUpperCase()}
           </span>
         </div>
--- my-personal-website/tailwind.config.js
+++ my-personal-website/tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'inter': ['Inter', 'sans-serif'],
-        sans: ['Inter', 'sans-serif'],
+        inter: ["Inter", "sans-serif"],
+        sans: ["Inter", "sans-serif"],
+      },
+      spacing: {
+        "1/4": "25%",
+        "3/4": "75%",
+      },
+      colors: {
+        'slate': "#8892b0",
+      },
     },
-    spacing: {
-      '1/4': '25%',
-      '3/4': '75%',
-    },
+    plugins: [],
   },
-  plugins: [],
-}
-}
+};
