ff6281a74f4530161e3a43dca17eb7e9195c3e78
MicahKepe <mik3@rice.edu>
2023-06-19 08:56:37 -0600

    Changed info in contact section

--- src/icons/LinkedinLogo.js
+++ src/icons/LinkedinLogo.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const LinkedinLogo = ({ fill }) => (
+const LinkedinLogo = ({ fill, className }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
     fill={fill}
     viewBox="0 0 455 455"
+    className={className}
   >
     <path
       fillRule="evenodd"
