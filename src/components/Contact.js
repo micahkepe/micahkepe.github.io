22bb590bedd9e164ae8328b3e043027f75dfe61d
MicahKepe <mik3@rice.edu>
2023-06-22 21:04:35 -0600

    Remove contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ContactForm from "./ContactForm";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import LinkedinLogo from "../icons/LinkedinLogo";
 
 const Contact = ({ windowWidth }) => {
   const paddingClass = windowWidth > 768 ? "pt-8" : "";
@@ -8,13 +9,41 @@ const Contact = ({ windowWidth }) => {
       <div
         className={`contact-content font-inter text-base font-thin text-slate pr-8 pl-3 ${paddingClass}`}
       >
-      <ContactForm />
+        <dl className="mt-10 space-y-4 text-base leading-7 text-gray-300">
+          <div className="flex gap-x-4">
+            <dt className="flex-none">
+              <span className="sr-only">Email</span>
+              <EnvelopeIcon
+                className="h-7 w-6 text-gray-400"
+                aria-hidden="true"
+              />
+            </dt>
+            <dd>
+              <a className="hover:text-white" href="mailto:micahkepe@gmail.com">
+                micahkepe@gmail.com
+              </a>
+            </dd>
+          </div>
+          <div className="flex gap-x-4">
+            <dt className="flex-none">
+              <LinkedinLogo fill="#0e76a8" className="w-6 h-6" />{" "}
+              {/* LinkedIn's brand color is #0e76a8 */}
+            </dt>
+            <dd>
+              <a
+                href="https://www.linkedin.com/in/micah-kepe/"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:text-white"
+              >
+                Connect with me on LinkedIn!
+              </a>
+            </dd>
+          </div>
+        </dl>
       </div>
     </section>
   );
 };
 
 export default Contact;
-
-
-
