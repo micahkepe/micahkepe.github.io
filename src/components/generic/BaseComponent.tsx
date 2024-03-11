70395abe3f58d029fd5a0c4935a79a23be8da6fb
micahkepe <micahkepe@gmail.com>
2024-03-10 20:37:42 -0700

    Adjust margins between base components

--- src/components/generic/BaseComponent.tsx
+++ src/components/generic/BaseComponent.tsx
@@ -11,7 +11,7 @@ const BaseComponent: React.FC<BaseComponentProps> = ({
 }) => {
   // Use `flex-col` for mobile and `flex-row` for larger screens
   return (
-    <div className="flex flex-col sm:flex-row rounded-md p-3 mb-4 hover:bg-white/5">
+    <div className="flex flex-col sm:flex-row rounded-md p-3 mb-6 hover:bg-white/5">
       <div className="flex-shrink-0 font-semibold text-slate text-xs uppercase mb-4 sm:mb-0 sm:mr-4">
         {leftSideContent}
       </div>
