874917c5832778d0c3489c466c430976d40f7ba8
micahkepe <micahkepe@gmail.com>
2024-05-11 19:53:49 -0700

    refactor: Update SVG rendering in ExperienceComponent and ProjectComponent

--- src/components/generic/ExperienceComponent.tsx
+++ src/components/generic/ExperienceComponent.tsx
@@ -40,21 +40,23 @@ const ExperienceComponent: React.FC<ExperienceComponentProps> = ({
           rel="noreferrer"
           className="flex items-center space-x-2"
         >
-          <span>{title}</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-4 h-4 inline-block"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-            />
-          </svg>
+          <div>
+            <span>{title}</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-4 h-4 inline-block ml-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+              />
+            </svg>
+          </div>
         </a>
       </div>
       <p className="font-thin text-sm text-slate mt-4 mb-4">{description}</p>
