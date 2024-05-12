eaf2b9a63f227de0e4afcd2d3129b79dc88d784e
micahkepe <micahkepe@gmail.com>
2024-05-11 17:23:27 -0700

    Updated JSX to render latest blog posts with title, publication date, and content; un-hide old blog hyperlink

--- src/components/views/Blog.tsx
+++ src/components/views/Blog.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 
 interface BlogProps {
   windowWidth: number;
@@ -6,13 +6,50 @@ interface BlogProps {
 
 interface BlogPost {
   title: string;
-  date: string;
-  excerpt: string;
-  url: string;
+  link: string;
+  pubDate: string;
+  content: string;
 }
 
 const Blog: FC<BlogProps> = ({ windowWidth }) => {
   const paddingClass = windowWidth > 768 ? "pt-8" : "";
+  const [blogPosts, setBlogPosts] = useState<BlogPost[]>([]);
+
+  useEffect(() => {
+    const fetchBlogPosts = async () => {
+      try {
+        // Fetch blog posts from the Atom feed of the Programming category
+        const response = await fetch(
+          "https://micahkepe.com/blog/categories/programming/atom.xml",
+        );
+        const xmlText = await response.text();
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlText, "text/xml");
+        const entries = xmlDoc.getElementsByTagName("entry");
+        const posts = Array.from(entries).map((entry) => ({
+          title: entry
+            .getElementsByTagName("title")[0]
+            .textContent.replace(/^\[\d+\]\s*/, ""), // Remove post number prefix
+          link: entry.getElementsByTagName("link")[0].getAttribute("href"),
+          pubDate: entry.getElementsByTagName("published")[0].textContent,
+          content: entry.getElementsByTagName("summary")[0].textContent,
+        }));
+
+        // Sort blog posts by publication date
+        posts.sort((a, b) => {
+          return new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime();
+        });
+
+        // Limit the number of blog posts to display (adjust as needed)
+        posts.splice(3);
+        setBlogPosts(posts);
+      } catch (error) {
+        return;
+      }
+    };
+
+    fetchBlogPosts();
+  }, []);
 
   return (
     <section id="about">
@@ -21,13 +58,56 @@ const Blog: FC<BlogProps> = ({ windowWidth }) => {
       >
         <br />
         <p className="indent-8">
-          My blog is a collection of my thoughts and experiences as a computer
-          science student at Rice University and as a software engineer. I write
-          about technical topics ranging from theory and algorithms to software
-          development and data science. I also explore potpourri topics such as
-          information security, productivity, and personal development.
+          My &quot;secret&quot; blog is a collection of my thoughts and
+          experiences as a computer science student at Rice University and as a
+          software engineer. I write about technical topics ranging from theory
+          and algorithms to software development and data science. I also
+          explore potpourri topics such as information security, productivity,
+          and personal development. Here&apos;s the latest programming-related
+          blog posts:
         </p>
-        <div id="example-blog-posts" className="mt-5"></div>
+        <br />
+        <div id="latest-blog-posts" className="mt-2 ml-5">
+          {blogPosts.map((post) => (
+            <div
+              key={post.link}
+              className="mb-4 border-solid border-2 border-slate p-4 rounded-lg hover:bg-white/5"
+            >
+              <h3 className="text-sm text-white font-semibold mb-2">
+                {post.title}
+              </h3>
+              <p className="text-sm font-semibold text-gray-500 mb-4">
+                {new Date(post.pubDate).toLocaleDateString()}
+              </p>
+              <div
+                className="mt-2 text-sm text-slate"
+                dangerouslySetInnerHTML={{ __html: post.content }}
+              ></div>
+              <div className="flex justify-end">
+                <a
+                  href={post.link}
+                  className="text-sm text-white hover:text-green decoration-green"
+                >
+                  Read More
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-4 h-4 inline-block"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
         <br />
       </div>
       <div>
