9a96627950a276f1b5e79039ec53a12608934aab
micahkepe <micahkepe@gmail.com>
2025-02-25 13:51:05 -0600

    fix(Lighthouse): ensure <dd> items are properly encapsulated

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -8,7 +8,7 @@ const Contact: FC = () => {
     <section id="contact">
       <article className="contact-content pt-0 sm:pt-8 text-base font-thin text-slate pr-8 pl-3">
         <dl className="mt-10 space-y-4 text-base leading-7 text-gray-300">
-          <p className="flex gap-x-4">
+          <div className="flex gap-x-4">
             <dt className="flex-none">
               <span className="sr-only">Email</span>
               <EnvelopeIcon
@@ -24,13 +24,14 @@ const Contact: FC = () => {
                 micahkepe@gmail.com
               </a>
             </dd>
-          </p>
-          <p className="flex gap-x-4">
+          </div>
+
+          <div className="flex gap-x-4">
             <dt className="flex-none">
               <LinkedinLogo
                 fill="currentColor"
                 className="w-6 h-6 text-sky-700"
-              />{" "}
+              />
             </dt>
             <dd>
               <a
@@ -42,7 +43,7 @@ const Contact: FC = () => {
                 Connect with me on LinkedIn!
               </a>
             </dd>
-          </p>
+          </div>
         </dl>
       </article>
     </section>
