9988cf46cc8eb48cdbc45d9b4bb9f55ef0b4d25f
micahkepe <micahkepe@gmail.com>
2025-12-12 13:33:29 -0600

    chore: life updates

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -7,7 +7,7 @@ const About: FC = () => {
       <article className="about-content pt-0 sm:pt-8 text-base text-left font-normal text-slate ml-2 mr-2">
         <br />
         <p className="indent-8">
-          I am a computer science student at{" "}
+          I am a recent computer science student graduate from{" "}
           <a
             href="https://www.niche.com/colleges/rice-university/"
             target="_blank"
@@ -16,7 +16,7 @@ const About: FC = () => {
           >
             Rice University
           </a>
-          , where I am dedicated to obtaining a Bachelor of Science in{" "}
+          , where I obtained my Bachelor of Science in{" "}
           <a
             href="https://csweb.rice.edu/"
             target="_blank"
@@ -31,24 +31,13 @@ const About: FC = () => {
         </p>
         <br />
         <p className="indent-8">
-          I aspire to acquire the necessary technical skills from Rice
-          University to create significant and meaningful contributions that
-          address the ever-growing issues in our interconnected world. With a
-          relentless drive to continually push my boundaries, I am confident in
-          my ability to make a positive impact.
+          I aspire to acquire the necessary technical skills from my
+          professional experiences to create significant and meaningful
+          contributions that address the ever-growing issues in our
+          interconnected world. With a relentless drive to continually push my
+          boundaries, I am confident in my ability to make a positive impact.
         </p>
         <p className="indent-8"></p>
-        <br />
-        <p className="indent-8">
-          Throughout my academic journey, I am excited about establishing
-          connections with industry professionals, fellow students, and
-          potential mentors. I believe that by engaging in thought-provoking
-          discussions and exchanging ideas, we can collectively explore
-          opportunities within the realms of computer science and data science.
-          I am eager to participate in internships, research collaborations, and
-          networking opportunities that will further enrich my skills and allow
-          me to contribute to impactful projects.
-        </p>
         <br />
         <p className="indent-8">
           Please feel free to reach out to me via email or connect with me on
